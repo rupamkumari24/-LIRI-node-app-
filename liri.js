ce60079689dc05ce5302088fb1fb161600e0d3e0
rupamkumari24 <rupamkiran@gmail.com>
2018-02-14 23:51:31 -0500

    Fixed Bugs in liri.js

--- liri.js
+++ liri.js
@@ -1,24 +1,24 @@
 
 require("dotenv").config();
 
+var fs = require("fs"); //reads and writes files
+var request = require("request");
 
+//Import our Keys File
+var keys = require('./keys');
+//var keys = require("./keys.js");
 
-	//Grab data from keys.js
+var twitter = require("twitter");
 
-var keys = require('./keys.js');
-var request = require('request');
-//var twitter = require('twitter');
-var spotify = require('spotify');
-var client = new Twitter(keys.twitter);
-var fs = require('fs');
+//var spotify = require ("spotify");
+//Import the Spotify API
+var Spotify = require('node-spotify-api');
+ 
+//Create a Spotify Client
+var spotify = new Spotify(keys.spotifyKeys);
 
+var liriArgument = process.argv[2];
 
-
-//Stored argument's array
-
-var nodeArgv = process.argv;
-
-var command = process.argv[2];
 // ---------------------------------------------------------------------------------------------------------------
 	// Possible commands for this liri app
 	switch(liriArgument) {
@@ -27,12 +27,12 @@ var command = process.argv[2];
 		case "movie-this": movieThis(); break;
 		case "do-what-it-says": doWhatItSays(); break;
 		// Instructions displayed in terminal to the user
-		default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
-			"1. my-tweets 'any twitter name' " +"\r\n"+
-			"2. spotify-this-song 'any song name' "+"\r\n"+
-			"3. movie-this 'any movie name' "+"\r\n"+
-			"4. do-what-it-says."+"\r\n"+
-			"Be sure to put the movie or song name in quotation marks if it's more than one word.");
+		default: console.log("\r\nTry typing one of the following commands after 'node liri.js' : "+
+			"\r\n1. my-tweets 'any twitter name' " +
+			"\r\n2. spotify-this-song 'any song name' "+
+			"\r\n3. movie-this 'any movie name' "+
+			"\r\n4. do-what-it-says."+
+			"\r\nBe sure to put the movie or song name in quotation marks if it's more than one word.");
 	};
 // ---------------------------------------------------------------------------------------------------------------
 // Functions
@@ -48,16 +48,16 @@ var command = process.argv[2];
 				var movieObject = JSON.parse(body);
 				//console.log(movieObject); // Show the text in the terminal
 				var movieResults =
-				"------------------------------ begin ------------------------------" + "\r\n"
-				"Title: " + movieObject.Title+"\r\n"+
-				"Year: " + movieObject.Year+"\r\n"+
-				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
-				"Country: " + movieObject.Country+"\r\n"+
-				"Language: " + movieObject.Language+"\r\n"+
-				"Plot: " + movieObject.Plot+"\r\n"+
-				"Actors: " + movieObject.Actors+"\r\n"+
-				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
-				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+				"------------------------------ begin ------------------------------" + 
+				"\nTitle: " + movieObject.Title+
+				"\r\nYear: " + movieObject.Year+
+				"\r\nImdb Rating: " + movieObject.imdbRating+
+				"\r\nCountry: " + movieObject.Country+
+				"\r\nLanguage: " + movieObject.Language+
+				"\r\nPlot: " + movieObject.Plot+
+				"\r\nActors: " + movieObject.Actors+
+				"\r\nRotten Tomatoes Rating: " + movieObject.tomatoRating+
+				"\r\nRotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
 				"------------------------------ fin ------------------------------" + "\r\n";
 				console.log(movieResults);
 				log(movieResults); // calling log function
@@ -70,10 +70,10 @@ var command = process.argv[2];
 	// Tweet function, uses the Twitter module to call the Twitter api
 	function myTweets() {
 		var client = new twitter({
-			consumer_key: keys.twitterKeys.consumer_key,
-			consumer_secret: keys.twitterKeys.consumer_secret,
-			access_token_key: keys.twitterKeys.access_token_key,
-			access_token_secret: keys.twitterKeys.access_token_secret, 
+			consumer_key: keys.twitter.consumer_key,
+			consumer_secret: keys.twitter.consumer_secret,
+			access_token_key: keys.twitter.access_token_key,
+			access_token_secret: keys.twitter.access_token_secret, 
 		});
 		var twitterUsername = process.argv[3];
 		if(!twitterUsername){
@@ -98,14 +98,15 @@ var command = process.argv[2];
 			}
 		});
 	}
-	// Spotify function, uses the Spotify module to call the Spotify api
-	function spotifyThisSong(songName) {
+	// Spotify function, uses the Spotify module to call the Spotify api	
+
+	function spotifyThisSong(songName) {		
 		var songName = process.argv[3];
 		if(!songName){
 			songName = "The Sign";
 		}
 		params = songName;
-		spotify.search({ type: "track", query: params }, function(err, data) {
+		spotify.search({type: "track", query: params }, function(err, data) {
 			if(!err){
 				var songInfo = data.tracks.items;
 				for (var i = 0; i < 5; i++) {
@@ -126,6 +127,8 @@ var command = process.argv[2];
 			}
 		});
 	};
+
+
 	// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
 	function doWhatItSays() {
 		fs.readFile("random.txt", "utf8", function(error, data){
